5ec349fddb51e700109ed76e56d0509487c31a1d
David Kim <davidjinuk90@gmail.com>
2017-01-12 20:17:13 +0000

    Added function to make calculations

--- results.js
+++ results.js
@@ -3,7 +3,17 @@ var input = [
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-var result = input.map(/* Your code here */);
+var result = input.map(function(point){
+  var z;
+
+    z = Math.pow(point.x, 2) + Math.pow(point.y, 2);
+    z = Math.sqrt(z);
+
+  return z;
+});
+
+console.log(result);
+
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
